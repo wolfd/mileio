df3f253b1f3b1c6af9903f5ff43672d6796e7c98
wolfd <2dawolf@gmail.com>
2016-10-23 19:43:11 -0400

    MapView in card works now. Add api key to api_keys.xml to experience fun.

--- MileIO/app/src/main/java/io/mile/mileio/MainActivity.java
+++ MileIO/app/src/main/java/io/mile/mileio/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -61,6 +62,8 @@ public class MainActivity extends AppCompatActivity {
                 email = firebaseUser.getEmail();
             }
         }
+
+        MapsInitializer.initialize(this);
     }
 
     @Override
--- MileIO/app/src/main/java/io/mile/mileio/TripAdapter.java
+++ MileIO/app/src/main/java/io/mile/mileio/TripAdapter.java
@@ -1,13 +1,15 @@
 package io.mile.mileio;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -22,11 +24,12 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements OnMapReadyCallback {
         // each data item is just a string in this case
         public TextView operatorNameView;
         public TextView distanceView;
         public MapView mapView;
+        public GoogleMap googleMap;
 
         public ViewHolder(View convertView) {
             super(convertView);
@@ -34,6 +37,17 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
             operatorNameView = (TextView) convertView.findViewById(R.id.operator_name_view);
             distanceView = (TextView) convertView.findViewById(R.id.distance_view);
             mapView = (MapView) convertView.findViewById(R.id.map_view);
+
+            if (mapView != null) {
+                mapView.onCreate(null);
+                mapView.onResume();
+                mapView.getMapAsync(this);
+            }
+        }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            this.googleMap = googleMap;
         }
     }
 
@@ -63,6 +77,20 @@ public class TripAdapter extends RecyclerView.Adapter<TripAdapter.ViewHolder> {
         holder.operatorNameView.setText(
                 trip.driver.getDisplayName()
         );
+
+        if (holder.googleMap != null) {
+            holder.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+    @Override
+    public void onViewRecycled(ViewHolder holder)
+    {
+        if (holder.googleMap != null)
+        {
+            holder.googleMap.clear();
+            holder.googleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        }
     }
 
     @Override
