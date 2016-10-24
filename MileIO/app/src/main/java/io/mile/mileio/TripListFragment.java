87a314ff09f8c822758c78d4fdb667cd48e2cb0e
wolfd <2dawolf@gmail.com>
2016-10-24 18:57:44 -0400

    Change format for Trip.

--- MileIO/app/src/main/java/io/mile/mileio/TripListFragment.java
+++ MileIO/app/src/main/java/io/mile/mileio/TripListFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -43,14 +44,14 @@ public class TripListFragment extends Fragment {
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        trips.add(new Trip(user, 10, new Date()));
-        trips.add(new Trip(user, 5.4, new Date()));
-        trips.add(new Trip(user, 25.8, new Date()));
-        trips.add(new Trip(user, 25.8, new Date()));
-        trips.add(new Trip(user, 25.8, new Date()));
-        trips.add(new Trip(user, 25.8, new Date()));
-        trips.add(new Trip(user, 25.8, new Date()));
-        trips.add(new Trip(user, 25.8, new Date()));
+        trips.add(new Trip(user, 10, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 5.4, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 25.8, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 25.8, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 25.8, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 25.8, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 25.8, new Date(), new ArrayList<LatLng>()));
+        trips.add(new Trip(user, 25.8, new Date(), new ArrayList<LatLng>()));
     }
 
     @Override
